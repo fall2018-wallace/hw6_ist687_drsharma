1f4253d5d575b311512206b98ef3f17754115a78
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:16:00 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,12 +1,19 @@
 
-myfunc <- function()
-{
-  census <- census[-c(53),]
+census <- census[-c(53),]
   census <- census[-c(1),]
   census <- census[-c(1,1:4)]
   colnames(census, do.NULL = TRUE)
   colnames(census) <- c("Statename", "population", "popOver18", "percentOver18")
+myfunc <- function()
+{
+  
   return(census)
 }
 
 myfunc()
+
+#arrests=cbind(rownames(arrests),arrests)
+#colnames(arrests)= c("StateName")
+#arrests
+#merged_data=merge(arrests,census)
+#colnames(merged_data)=c("Statename","Murder","Assault","UrbanPop", "Rape","population","popover18", "percentover18")
