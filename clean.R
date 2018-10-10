082bf22924fc953faeb8f169ab239c6ae6a7b81d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:18:42 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -20,3 +20,4 @@ colnames(arrests)= c("Statename")
 arrests
 merged_data=merge(arrests,census)
 colnames(merged_data)=c("Statename","Murder","Assault","UrbanPop", "Rape","population","popover18", "percentover18")
+merged_data
