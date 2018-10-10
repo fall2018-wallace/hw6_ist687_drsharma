d1fd70d583d4578df90c972754da3ffa635f9b43
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 16:07:39 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -6,4 +6,4 @@ merged_data=merge(arrests,cleaned)
 colnames(merged_data)=c("Statename","Murder","Assault","UrbanPop", "Rape","population","popover18", "percentover18")
 merged_data
 
-merge_1 <- merged_data
+
