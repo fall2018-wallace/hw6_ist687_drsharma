d41bc731d472e2592edb2d939b1964c2eaf36de9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:54:37 -0400

    Saving StepB5A.R

--- StepB5A.R
+++ StepB5A.R
@@ -1,6 +1,4 @@
 
-library(ggplot2)
+#library(ggplot2)
 
-BPop <- ggplot(BoxPop,aes(x=factor(0),y=population))
-
-#+geom_boxplot()
+BPop <- ggplot(BoxPop,aes(x=factor(0),y=population))+geom_boxplot()
