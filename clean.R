9ce287fbcf2d71198b5596c83a6e68bf54d1ddd4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:17:49 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -15,8 +15,9 @@ myfunc()
 
 census
 
+arrests <- USArrests
 arrests=cbind(rownames(arrests),arrests)
-colnames(arrests)= c("StateName")
+colnames(arrests)= c("Statename")
 arrests
 merged_data=merge(arrests,census)
 colnames(merged_data)=c("Statename","Murder","Assault","UrbanPop", "Rape","population","popover18", "percentover18")
