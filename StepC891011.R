c01bbc9d2b9d2362c45909e116515c761cd91110
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 16:23:21 -0400

    Saving StepC891011.R

--- StepC891011.R
+++ StepC891011.R
@@ -2,7 +2,10 @@
 library(ggplot2)
 
 
-MurderPerState <- ggplot(BarChartMurder,aes(x=Statename,y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
+MurderPerState <- ggplot(BarChartMurder,aes(x=Statename,y=no_of_murders))+geom_col()
+
+
+MurderPerStateTextRotated <- ggplot(BarChartMurder,aes(x=Statename,y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
 
 
 BMurderPerState <- ggplot(BarChartMurder,aes(x=reorder(Statename, no_of_murders),y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
