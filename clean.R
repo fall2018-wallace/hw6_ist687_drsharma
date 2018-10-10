efce9ef2e88e2bd39ebae2f56ae69449f78a9d6d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 23:08:19 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -5,8 +5,10 @@ USArrests
 arrests <- USArrests
 
 myfunc <- function()
-census <- census[-c(53),]
-census <- census[-c(1),]
-census <- census[-c(1,1:4)]
-colnames(census, do.NULL = TRUE)
-colnames(census) <- c("StateName", "population", "popOver18", "percentOver18")
+{
+    census <- census[-c(53),]
+    census <- census[-c(1),]
+    census <- census[-c(1,1:4)]
+    colnames(census, do.NULL = TRUE)
+    colnames(census) <- c("StateName", "population", "popOver18", "percentOver18")   
+}
