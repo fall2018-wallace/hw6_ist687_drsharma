b9ddfe42803b07d72a10ea48b61fc7f00ea703c7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 23:04:55 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -4,3 +4,8 @@
 USArrests
 arrests <- USArrests
 
+census <- census[-c(53),]
+census <- census[-c(1),]
+census <- census[-c(1,1:4)]
+colnames(census, do.NULL = TRUE)
+colnames(census) <- c("StateName", "population", "popOver18", "percentOver18")
