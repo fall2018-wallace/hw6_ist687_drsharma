7c45419a3a74a96bc3fe5a75d8905228d23c8d55
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 16:32:51 -0400

    Saving StepB4B.R

--- StepB4B.R
+++ StepB4B.R
@@ -4,3 +4,5 @@ library(ggplot2)
 HMurder <- ggplot(HistMurder,aes(x=Murder))+geom_histogram(bins=50,color="black", fill="blue")
 
 HPop <- ggplot(HistPop,aes(x=population))+geom_histogram(bins=50,color="black", fill="black")
+
+BPop <- ggplot(BoxPop,aes(x=factor(0),y=population))+geom_boxplot()
