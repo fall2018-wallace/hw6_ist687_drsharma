fc1503a8930799f785ce39b5e029ee12f2af9b93
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:19:58 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -1,5 +1,2 @@
 
-cbind(rownames(USArrests),cleaned)
-colnames(USArrests)= c("Statename")
-merged_data=merge(USArrests, cleaned)
-colnames(merged_data)=c("Statename","Murder","Assault","UrbanPop", "Rape","population","popover18", "percentover18")
+merged
