84a3ac11a232939939961d3ce78f2e8da19e1f0b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:40:05 -0400

    Saving StepB4A.R

--- StepB4A.R
+++ StepB4A.R
@@ -1,3 +1,3 @@
 
 library(ggplot2)
-Hpop <- ggplot(HistPop,aes(x=population))+geom_histogram(bins=50,color="black", fill="black")
+HPop <- ggplot(HistPop,aes(x=population))+geom_histogram(bins=50,color="black", fill="black")
