61b646c23163be49b6f35ade8f51ffbbeb021a09
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:56:37 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,10 @@
 
-census
+myfunc <- function()
+{
+  census <- census[-c(53),]
+  census <- census[-c(1),]
+  census <- census[-c(1,1:4)]
+  colnames(census, do.NULL = TRUE)
+  colnames(census) <- c("StateName", "population", "popOver18", "percentOver18")
+  return(census)
+}
