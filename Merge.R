b1c96ca2c3edc6241e0948d5175a7a9c63b65abe
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 01:03:06 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -2,7 +2,8 @@
 #Copying USArrests data set into a local variable arrests
 arrests <- USArrests
 
-#Below line of codes are used to merge the data frames USArrests and arrests using merge command and storing the merged data frame into another variable merged_data
+#Below line of codes are used to merge the data frames USArrests and arrests using merge command and 
+#storing the merged data frame into another variable merged_data
 arrests=cbind(rownames(arrests),arrests)
 colnames(arrests)= c("Statename")
 merged_data=merge(arrests,cleaned)
