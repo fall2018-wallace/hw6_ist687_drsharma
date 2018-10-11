510250dfd5bec66013ee9397684fceecbba4cd94
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 01:15:46 -0400

    Saving StepC891011.R

--- StepC891011.R
+++ StepC891011.R
@@ -1,10 +1,11 @@
 
+#Reading the library ggplot2
 library(ggplot2)
 
-
+#Calculating the Murders per state and representing them in the form of bar chart using the geom_col() function
 MurderPerState <- ggplot(BarChartMurder,aes(x=Statename,y=no_of_murders))+geom_col()
 
-
+#calculating the Murders per state but by displaying the x-axis labels in the rotated way so that the labels can be easily understood
 MurderPerStateTextRotated <- ggplot(BarChartMurder,aes(x=Statename,y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
 
 
