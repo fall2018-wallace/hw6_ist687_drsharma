0c74f307e6c0dac736dfdfae83633d954f9b5146
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 01:11:52 -0400

    Saving StepB4A.R

--- StepB4A.R
+++ StepB4A.R
@@ -4,9 +4,11 @@ library(ggplot2)
 
 #Below line of code generates a histogram for Murder in which x-axis displays the murders and y-axis displays the count of murders
 HMurder <- ggplot(merged_data,aes(x=Murder))+geom_histogram(bins=50,color="black", fill="blue")
+
 #Below line of code generates a histogram for Population in which x-axis displays the Population and y-axis displays the count of Population
 HPop <- ggplot(merged_data,aes(x=population))+geom_histogram(bins=50,color="black", fill="black")
 
+#Below line of code generates a boxplot representing the population
 BPop <- ggplot(merged_data,aes(x=factor(0),y=population))+geom_boxplot()
 
 BMurder <- ggplot(merged_data,aes(x=factor(0),y=Murder))+geom_boxplot()
