0ce7ded084c797bc44a628a1ddc07f6a4b137e37
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 01:06:26 -0400

    Saving StepD.R

--- StepD.R
+++ StepD.R
@@ -2,4 +2,5 @@
 #Reading the library ggplot2
 library(ggplot2)
 
+#Generating the scatter plot
 ScatterPlot <- ggplot(ScatterPlot, aes(x=population, y=percentover18)) + geom_point(aes(size=Murder, color=Murder))
